f86bb78e9ad6cb8c0e587f5f90b0e4d34e43e5a1
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2026-07-04 15:38:28 +0500

    Introduction to React Native , How React Native Works behind the scenes , Introduction to Expo , Runnig App and Displaying using result using Expo Go App ,  What is EAS , How to convert React Native Project into a downloadable app and deploying downloadable files for App using EAS built.

---
+++ First Mobile App/MyFirstApp/App.js
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function App() {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "green",
+  },
+});
+
+export default App;
